6e97e0f0d4b956db32a9885235f76ca1375fa27e
Alireza <mr.toghyiani@icloud.com>
2021-09-21 01:21:49 +0430

    - remove storyboardd - make launch screen - open view controller in app

---
+++ Speechify Assessment/UIColorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColorExtension.swift
+//  Speechify Assessment
+//
+//  Created by Alireza on 9/21/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backgroundColor = UIColor(named: "backgroundColor")!
+}
