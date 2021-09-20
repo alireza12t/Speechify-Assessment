d2b0481924ddcfd22e6440bb1b09eafd27da2282
Alireza <mr.toghyiani@icloud.com>
2021-09-21 00:57:38 +0430

    Initial Commit

---
+++ Speechify Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speechify Assessment
+//
+//  Created by Alireza on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
